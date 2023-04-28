2009620565cdb1e47992496d7b840d202c300c8b
stea10 <119765400+stea10@users.noreply.github.com>
2023-04-28 19:04:24 -0400

    Create login-submit.php

---
+++ login-submit.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "database_name";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+//check if  exists and the password is correct
+$sql = "SELECT * FROM users WHERE username = '$username'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    if (password_verify($password, $row["password"])) {
+        
+        $_SESSION["user_id"] = $row["id"];
+
+        // Redirecting to the buyer dashboard
+        header("Location: buyerdashboard.php");
+        exit();
+    } else {
+        // Display an error message to the user
+        echo "Invalid password";
+    }
+} else {
+    // Display an error message to the user
+    echo "Invalid username";
+}
+
+$conn->close();
+?>
