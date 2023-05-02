d6ed5fd3f32f98a64a2319d000f6e4c661e4e11f
bgula5 <blakegula001@gmail.com>
2023-05-02 00:57:42 -0400

    im stupid

--- houseInfo.php
+++ houseInfo.php
@@ -104,35 +104,7 @@ if (isset($_SESSION['user']) && isset( $_GET['house_id'])) {
   exit();
 }
 ?>
-
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Home</title>
-    <link rel="stylesheet" href="houseInfo.css" />
-  </head>
-  <body>
-    <header>
-      <div class="title">
-        <img class="logo" src="./logo.jpg" alt="Logo" />
-        <span>PROPERLY</span>
-      </div>
-      <nav>
-        <a href="./main.php" class="nav-item">Home</a>
-        <a href="./About.html" class="nav-item">About Us</a>
-        <form method="post" action="./logout.php">
-          <input
-            class="nav-item logout"
-            type="submit"
-            name="logout"
-            value="Logout"
-          />
-        </form>
-      </nav>
-    </header>
-    <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
